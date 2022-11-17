e202e1d4e0853a27feaf2e427a238d67e59cd856
LeonardiGaston <gaston.leonardi@mercadolibre.com>
2022-11-17 16:43:26 -0300

    Update Class Repository

--- src/main/java/com/example/sprint/repository/UserBuyerRepositoryImp.java
+++ src/main/java/com/example/sprint/repository/UserBuyerRepositoryImp.java
@@ -2,4 +2,23 @@ package com.example.sprint.repository;
 
 
 public class UserBuyerRepositoryImp implements IUserBuyerRepository{
+    @Override
+    public void create(Object o) {
+
+    }
+
+    @Override
+    public void read(Object o) {
+
+    }
+
+    @Override
+    public void update(Object o) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
 }
